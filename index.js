73eaae68bd3a363a91d1d3af6abd25a3360a47fc
Erik Johansson <erik.johansson87@gmail.com>
2016-07-13 14:11:33 +0200

    Removed unsupported module-linking of source files

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-process.env.NODE_PATH = __dirname;
-require('module').Module._initPaths();
-
-var parse = require('src/parse'),
-	stringify = require('src/stringify');
+var parse = require('./src/parse'),
+	stringify = require('./src/stringify');
 
 JSON.tryParse = parse;
 JSON.tryStringify = stringify;
