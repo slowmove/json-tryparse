1007ace520b60d8e3e9dae050e56a9204fe199d3
Erik Johansson <erik.johansson87@gmail.com>
2016-07-22 10:32:23 +0200

    updated tests, bumped version

--- test/jsonTest.js
+++ test/jsonTest.js
@@ -1,22 +1,26 @@
 var assert = require('chai').assert;
 require('../index');
 
+var obj = {
+    foo: "bar"
+};
+var strObj = JSON.stringify(obj);
+
 describe('Json', function() {
     describe('tryParse', function() {
         it('Should return the object even if trying to parse existing object', function() {
-            var obj = {
-                foo: "bar"
-            };
             assert.equal(obj, JSON.tryParse(obj));
+        });
+        it('Should return the object trying to parse a stringified json', function() {
+            assert.isObject(JSON.tryParse(strObj));
         });
     });
     describe('tryStringify', function() {
         it('Should return the stringified object even if already stringified', function() {
-            var obj = {
-                foo: "bar"
-            };
-            var strObj = JSON.stringify(obj);
             assert.equal(strObj, JSON.tryStringify(strObj));
+        });
+        it('Should return the stringified object trying to parse a json object', function() {
+            assert.equal(strObj, JSON.tryStringify(obj));
         });
     });
 });
